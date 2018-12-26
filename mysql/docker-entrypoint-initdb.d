bbdcc93ad2fb593f598ec66b38559f5f18e262af
srini0x00 <srini0x00@gmail.com>
2018-12-26 12:22:04 +0800

    added MySQL

---
+++ mysql/docker-entrypoint-initdb.d
@@ -0,0 +1,12 @@
+USE `users`;
+ 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+ 
+CREATE TABLE `users` (
+  `ID` int(10) NOT NULL,
+  `USERNAME` varchar(20) NOT NULL,
+  `PASSWORD` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `users` (`ID`, `USERNAME`, `PASSWORD`) VALUES (1, 'admin','admin'); 
