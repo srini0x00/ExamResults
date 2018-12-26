0fd69f15bc7d5f598fab086610408d928ca805e7
srini0x00 <srini0x00@gmail.com>
2018-12-26 19:28:29 +0800

    added MySQL

--- src/com/examresults/Database.java
+++ src/com/examresults/Database.java
@@ -8,6 +8,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletResponse;
 import javax.servlet.ServletResponseWrapper;
